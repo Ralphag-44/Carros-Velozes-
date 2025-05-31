bc50104c6aa6f5420cea27af8618a87a61924fd8
Rafael-AlmeidaR <rafael.almeida22@yahoo.com>
2025-05-30 22:14:33 -0300

    baseCamera

--- Camera.js
+++ Camera.js
@@ -1,5 +1,26 @@
 class Camera
-{   constructor()
-    {   
+{   constructor(x, y,width, height, xScale, yScale, following)
+    {   this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.xScale = xScale;
+        this.yScale = yScale;        
+        this.following = following;
+    }    
+    draw()
+    {   context.drawImage(background, 0,0,canvas.width, canvas.height);
+    }
+    show(points)
+    {   let onCamera = false;
+        for(let i = 0; (i < points.length); i++)
+        {   onCamera = (points[i].x >= this.x && points[i].x <= this.x+this.width)&&(points[i].y >= this.y && points[i].y <= this.y+this.height);
+        }
+        return onCamera;
+    }
+    update()
+    {   let followingCenter = cars[this.following].center();
+        this.x = followingCenter.x - this.width/2;
+        this.y = followingCenter.y - this.height/2;
     }
 }
--- Car.js
+++ Car.js
@@ -1,11 +1,23 @@
 class Car extends Entity
-{   constructor(points, img)
+{   constructor(points, img, id)
     {   
         super(points, img);
         this.angle = 0;
+        this.id = id;
+        this.camera = new Camera(this.x, this.y, canvas.width/4, canvas.height/4, 2, 2, id);
+    }
+    draw()
+    {   context.drawImage(this.img, this.points[0].x, this.points[0].y, this.points[1].x-this.points[0].x, this.points[2].y-this.points[0].y);
+        context.beginPath();
+        context.moveTo(this.points[0].x, this.points[0].y);
+        for(let i = 1; (i < this.points.length); i++)
+        {   context.lineTo(this.points[i].x, this.points[i].y);
+        }
+        context.closePath();
+        context.stroke();
     }
     update()
-    {
+    {   
     }
     rotate(angle)
     {   this.angle += angle;
@@ -17,5 +29,15 @@ class Car extends Entity
     {   for(let i = 0; (i < this.points.length); i++)
         {   this.points[i].translate(dx, dy);
         }
+    }
+    center()
+    {   let center = new Point(0, 0);
+        for(let i = 0; (i < this.points.length); i++)
+        {   center.x += this.points[i].x;
+            center.y += this.points[i].y;
+        }
+        center.x /= this.points.length;
+        center.y /= this.points.length;
+        return center;
     }
 }
